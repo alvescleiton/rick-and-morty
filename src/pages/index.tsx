4f1eafc73443ad5e249c436c77d19d3891b630b3
Cleiton Alves <cleiton_jc@yahoo.com.br>
2023-09-20 20:55:27 -0300

    title on main page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,12 +11,31 @@ const Home: NextPage = () => {
           blur-sm 
           brightness-150 
           -z-10
-          animate-mypulse
+          animate-myhue
         " 
         src="/assets/main.png" 
         alt="Rick and Morty" 
         layout="fill"
       />
+
+      <p className="
+        self-center 
+        text-purple-800 
+        text-4xl/[40px] 
+        lg:text-5xl/[50px] 
+        text-center
+        bg-white
+        bg-opacity-50
+        w-full
+        py-10
+        shadow-xl
+        backdrop-blur-[2px]
+        -skew-y-3
+      ">
+        Rick
+        <small className="block py-2 text-xl lg:text-3xl">and</small>
+        Morty
+      </p>
     </div>
   )
 }
