c9f1ddd1f6c760f017d82fce2c026cc60a2a1ec2
Cleiton Alves <cleiton_jc@yahoo.com.br>
2023-09-28 19:21:20 -0300

    character component

---
+++ src/components/Character/index.tsx
@@ -0,0 +1,34 @@
+import Image from 'next/image'
+
+interface Character {
+  name: string
+  image: string
+  gender: string
+  status: string
+}
+
+interface Props {
+  key: string | number
+  character: Character
+}
+
+export function Character({ key, character }: Props) {
+  return (
+    <div key={key} className="aspect-square bg-slate-100 rounded-md">
+      <div className="relative w-full aspect-square">
+        <Image 
+          src={character?.image}
+          alt={character?.name}
+          layout="fill"
+          className="rounded-t-md"
+          priority
+        />
+      </div>
+      <div className="text-xs sm:text-sm text-slate-500 p-2">
+        <h3 className="text-slate-800">{character?.name}</h3>
+        <span className="block">Gender: {character?.gender}</span>
+        <span className="block">Status: {character?.status !== "unknown" ? character?.status : "?"}</span>
+      </div>
+    </div>
+  )
+}
